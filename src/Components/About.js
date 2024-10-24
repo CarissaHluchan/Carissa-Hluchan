3627b1d57c2407d1de4a50ed57436b73e476cd37
Carissa Hluchan <carissahluchan@gmail.com>
2024-10-23 20:01:37 -0600

    Add: skills logos and start of Json file

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,7 +11,7 @@ function About() {
       <p>During this transition, I discovered my passion for building engaging and dynamic user experiences. I thrive in environments that push me to be both persistent and creative, tackling problems from different angles to find effective solutions. Through my work at Turing, I’ve honed my skills in software testing practices such as TDD, unit testing, and Cypress, and I’ve developed a deep appreciation for collaborative problem-solving in group projects.</p>
       <p>I’m especially passionate about styling and UI design—creating visually compelling, intuitive interfaces that delight users. Whether it’s crafting responsive layouts or exploring innovative CSS techniques, I love using my creativity to bring ideas to life.</p>
       <p>When I’m not coding, you’ll find me outdoors—hiking, rock climbing, and split boarding. The challenge and creativity I find in outdoor adventures often fuel my problem-solving approach as a developer.</p>
-      <p>Let’s connect! Explore my porflio for more on my projects, and feel free to message me to discuss potential collaborations or opportunities.</p>
+      <p>Let’s connect! Explore my portflio for more on my projects, and feel free to message me to discuss potential collaborations or opportunities.</p>
     </section>
   )
 }
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,14 +10,93 @@ function Skills() {
         <div className='skills-title'>Languages/Frameworks:</div>
         <p> React, React-Router, JavaScript, TypeScript, HTML5, CSS3</p>
 
+        <div className='skill-wrapper'>
+          <i class="devicon-javascript-plain"></i>
+          <i class="devicon-javascript-plain colored"></i>
+          <div>JavaScript</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-react-original"></i>
+          <i class="devicon-react-original colored"></i>
+          <div>React</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-reactrouter-plain"></i>
+          <i class="devicon-reactrouter-plain colored"></i>
+          <div>React-Router</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-typescript-plain"></i>
+          <i class="devicon-typescript-plain colored"></i>
+          <div>TypeScript</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-html5-plain"></i>
+          <i class="devicon-html5-plain colored"></i>
+          <div>HTML</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-css3-plain"></i>
+          <i class="devicon-css3-plain colored"></i>
+          <div>CSS</div>
+        </div>
+
         <div className='skills-title'>Testing:</div>
-        <p>Test-Driven Development (TDD), Unit Testing (Mocha, Chai), Cypress E2E</p>
+        <p>Test-Driven Development (TDD)</p>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-mocha-plain"></i>
+          <i class="devicon-mocha-plain colored"></i>
+          <div>Unit Testing (Mocha, Chai),</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-cypressio-plain"></i>
+          <i class="devicon-cypressio-plain colored"></i>
+          <div>Cypress</div>
+        </div>
 
         <div className='skills-title'>Workflow:</div>
-        <p> Agile Workflow, GitHub, GitHub project board, GitHub issues, npm, Vite.js, Miro, Canva</p>
+        <p> Agile Workflow, GitHub project board, GitHub issues, npm, Vite.js, Miro, Canva</p>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-github-original"></i>
+          <i class="devicon-github-original colored"></i>
+          <div>GitHub</div>
+
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-npm-original-wordmark"></i>
+          <i class="devicon-npm-original-wordmark colored"></i>
+          <div>npm</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-vitejs-plain"></i>
+          <i class="devicon-vitejs-plain colored"></i>
+          <div>Vite.js</div>
+        </div>
 
         <div className='skills-title'>Design:</div>
-        <p>Adobe Photoshop, InDesign, Lightroom, Color Theory, Accessibility, UI/UX, Responsive Design </p>
+        <p>Abobe InDesign, Abobe Lightroom, Color Theory, Accessibility, UI/UX, Responsive Design </p>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-illustrator-plain"></i>
+          <i class="devicon-vitejs-plain colored"></i>
+          <div>Adobe Illustrator</div>
+        </div>
+
+        <div className='skill-wrapper'>
+          <i class="devicon-photoshop-plain"></i>
+          <i class="devicon-photoshop-plain colored"></i>
+          <div>Adobe Photoshop</div>
+        </div>
       </div>
 
     </section>
