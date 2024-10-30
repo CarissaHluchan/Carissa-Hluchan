57280e844ffc7a9a709dc0f5f3c98f8eeef72cfa
Carissa Hluchan <carissahluchan@gmail.com>
2024-10-29 18:37:13 -0600

    Fix: Name with title and for dark mode

--- src/Components/Name/Name.js
+++ src/Components/Name/Name.js
@@ -1,36 +1,37 @@
-import React from 'react';
+// import React from 'react';
 
-import mountainsVideo from '../../assets/mountains.mp4';
-import nebulaVideo from '../../assets/nebula.mp4';
-import growingVideo from '../../assets/plant_growing.mp4';
-import earthVideo from '../../assets/space_earth.mp4';
-import './Name.css'
+// import mountainsVideo from '../../assets/mountains.mp4';
+// import nebulaVideo from '../../assets/nebula.mp4';
+// import growingVideo from '../../assets/plant_growing.mp4';
+// import earthVideo from '../../assets/space_earth.mp4';
+// import './Name.css'
 
-function Name() {
-  return (
-    <main className='name-section'>
-      <video autoPlay muted loop>
-        <source
-          src={nebulaVideo}
-          type="video/mp4"
-        />
-      </video>
-      <div className='name-container'>
-        <h1>Carissa Hluchan</h1>
-      </div>
-    </main>
-  )
-}
+// function Name() {
+//   return (
+//     <main className='name-section'>
+//       <video autoPlay muted loop>
+//         <source
+//           src={nebulaVideo}
+//           type="video/mp4"
+//         />
+//       </video>
+//       <div className='name-container'>
+//         <h1>Carissa Hluchan</h1>
+//       </div>
+//     </main>
+//   )
+// }
 
-export default Name
+// export default Name
 
-// import React, { useState, useEffect } from 'react';
+// import React from 'react';
+// import { useState, useEffect } from 'react';
 
-// import mountainsVideo from '../assets/mountains.mp4';
-// import nebulaVideo from '../assets/nebula.mp4';
-// import growingVideo from '../assets/plant_growing.mp4';
-// import earthVideo from '../assets/space_earth.mp4';
-// import './Name.css';
+// import mountainsVideo from '../../assets/mountains.mp4';
+// import nebulaVideo from '../../assets/nebula.mp4';
+// import growingVideo from '../../assets/plant_growing.mp4';
+// import earthVideo from '../../assets/space_earth.mp4';
+// import './Name.css'
 
 // const videos = [mountainsVideo, nebulaVideo, growingVideo, earthVideo];
 
@@ -40,7 +41,7 @@ export default Name
 //   useEffect(() => {
 //     const videoInterval = setInterval(() => {
 //       setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
-//     }, 20000); // Change video every 10 seconds
+//     }, 12000); // Change video every 10 seconds
 
 //     return () => clearInterval(videoInterval); // Cleanup interval on component unmount
 //   }, []);
@@ -54,11 +55,50 @@ export default Name
 //         />
 //       </video>
 //       <div className="name-container">
-//         <h1>Carissa Hluchan</h1>
-//         <h2>Hluchan</h2>
+//         <h1>Carissa Hluchan <br></br><span className='h1-span'>Web Developer</span></h1>
+//         {/* <h2>Hluchan</h2> */}
 //       </div>
 //     </main>
 //   );
 // }
 
 // export default Name;
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+import mountainsVideo from '../../assets/mountains.mp4';
+import nebulaVideo from '../../assets/nebula.mp4';
+import growingVideo from '../../assets/plant_growing.mp4';
+// import earthVideo from '../../assets/space_earth.mp4';
+import './Name.css'
+
+const videos = [mountainsVideo, nebulaVideo, growingVideo];
+
+function Name() {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  useEffect(() => {
+    const videoInterval = setInterval(() => {
+      setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
+    }, 12000); // Change video every 10 seconds
+
+    return () => clearInterval(videoInterval); // Cleanup interval on component unmount
+  }, []);
+
+  return (
+    <main className='name-section'>
+      <video autoPlay muted loop key={currentVideoIndex}>
+        <source
+          src={videos[currentVideoIndex]}
+          type="video/mp4"
+        />
+      </video>
+      <div className="name-container">
+        <h1><span className='carissa'>Carissa</span><span className='hluchan'>Hluchan</span><span className='h1-span'>Web Developer</span></h1>
+      </div>
+    </main>
+  );
+}
+
+export default Name;
