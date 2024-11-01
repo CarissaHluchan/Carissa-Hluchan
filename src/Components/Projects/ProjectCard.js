d5fda751f5ab2cc6201932eaf56453e3b712ea14
Carissa Hluchan <carissahluchan@gmail.com>
2024-11-01 12:52:29 -0600

    Add: more functionality and styling to ProjectCard and Project

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -1,30 +1,48 @@
-import { React, useState } from 'react';
+import { React, useState, useRef } from 'react';
 
 import Project from './Project';
 import './ProjectCard.css'
 
 function ProjectCard({ id, title, description, technologies, collaborators, projectNotes, repo, liveSite }) {
     
-    const [ showProject, setShowProject] = useState(false)
+    const [showProject, setShowProject] = useState(false);
+    const projectRef = useRef(null);
 
     const handleProjectClick = () => {
         setShowProject(true);
-    }
 
-    const techBox = technologies.map(tech => {
+        setTimeout(() => {
+            if (projectRef.current) {
+                projectRef.current.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+            } else {
+                console.log('projectRef is not set');
+            }
+        }, 100); 
+    };
+
+    const handleClickToClose = (event) => {
+        event.stopPropagation(); 
+        setShowProject(false);
+    };
+
+    const techBox = technologies.map((tech, index) => {
         return (
-            <div className='tech-box'>{tech}</div>
-        )
+            <img key={index} src={tech} alt={`Technology logo for ${technologies[index]}`} className={tech.includes('react-original') ? 'tech-image-dark-bg' : 'tech-image'} />
+        );
     });
 
     return (
         <div className='project-wrapper' id={id} onClick={handleProjectClick}>
-            <h3>{title}</h3>
+            <h3 className='title'>{title}</h3>
             <p className='project-description'>{description}</p>
-            <h4>Technologies:</h4>
+            <h4 className='tech'>Technologies:</h4>
             <div className='tech-box-wrapper'>{techBox}</div>
             {showProject && (
                 <Project 
+                    ref={projectRef} 
                     id={id}
                     title={title}
                     description={description}
@@ -33,10 +51,11 @@ function ProjectCard({ id, title, description, technologies, collaborators, proj
                     projectNotes={projectNotes}
                     repo={repo}
                     liveSite={liveSite}
+                    handleClickToClose={handleClickToClose}
                 />
             )}
         </div>
-    )
+    );
 }
 
-export default ProjectCard
+export default ProjectCard;
